cc93b130a6cc78437f0b749087b1884034e608de
FabriMig <miguel.arias.vargas02@gmail.com>
2025-03-21 21:19:23 -0600

    first commit

---
+++ src/data/projects.js
@@ -0,0 +1,8 @@
+export const projects = [
+    {
+        name: "Equation Solver",
+        description: "Equation web to solve first dagree equations created since 0",
+        techStack: ["typescript", "mongoDb", "react", "tailwind", "express"],
+        imageRoute: "/equationWeb.webp"
+    }
+]
